6febe196cbc8d3d4b54dc9b3e62076bd7fbcc958
KeabetsweMatlhabe <157456088+KeabetsweMatlhabe@users.noreply.github.com>
2024-05-09 20:23:20 +0200

    None numeric input check

--- StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
@@ -3,35 +3,14 @@ const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+
+  if (isNaN(dividend) || isNaN(divider)) { // checking if inputs are numbers.
+    result.classList.add("critical-error") // Changes HTML body to red.
+    result.innerText = "Something critical went wrong. Please reload the page";
+    console.error("Error: Non-numeric input provided");
 
 
-  let dividend;
-  let divider;
-  try {
-    dividend = parseInt(document.getElementById("dividend").ariaValueMax, 10);
-    divider = parseInt(document.getElementById("divider").ariaValueMax, 10);
-    if (isNaN(dividend)|| isNaN(divider)){
-      throw new Error ("Invalid input.Please enter numbers only");
-    } 
-  }catch (error) {
-    result.textContent = "Division not perfomed. Invalid number.Try again.";
-    console.error("Error:", error.message);
-    return;
   }
-
-  let divisionResult;
-  try{
-    if (divider === 0){
-      throw new Error ("Division by zero its not allowed.");
-    }else {
-      divisionResult = math.floor(diddend / divider);
-    }
-  }catch (error) {
-    result.textContent = "Division not performed.Result" + error.message + ".Try Again."
-    console.error("Error:",error.message );
-    return;
-
-    
-  }
- 
 });
